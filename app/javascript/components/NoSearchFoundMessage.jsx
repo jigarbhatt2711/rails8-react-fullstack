6b48b9e715cd88126389821c29863eebba81220d
jigar bhatt <jigar@iservegroup.com>
2026-05-08 15:12:46 +0530

    Fixed dom exception by adding extra <div></div>

--- app/javascript/components/NoSearchFoundMessage.jsx
+++ app/javascript/components/NoSearchFoundMessage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-const NoSearchFoundMessage = ({tag}) => {
+const NoSearchFoundMessage = ({ tag }) => {
   return (
-    <div className="mt-5 alert alert-danger alert-dismissible fade show" role="alert">
-      <strong>Oops!</strong> No Resul Found with {tag.label} Tag.
+    <div>
+      <div
+        className="mt-5 alert alert-danger alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>Oops!</strong> No Resul Found with {tag.label} Tag.
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        />
+      </div>
     </div>
   );
 };
